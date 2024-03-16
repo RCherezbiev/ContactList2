e74ac114dce06cfafa619e7576b526ef21a6c24d
Rustam Cherezbiev <rcherezbiev@gmai.com>
2024-03-16 20:35:31 +0300

    Initial Commit

---
+++ ContactList2/ContactList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList2App.swift
+//  ContactList2
+//
+//  Created by Rustam Cherezbiev on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactList2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactList2
+//
+//  Created by Rustam Cherezbiev on 16.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
